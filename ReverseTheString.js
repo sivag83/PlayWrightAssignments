8da5ba1d606573aea46c33dc696d473bee62f693
sivag83 <sivaganesh.lingadurai@gmail.com>
2025-09-23 23:33:07 +0530

    Day4 HomeworkAssignment Reverse string

---
+++ ReverseTheString.js
@@ -0,0 +1,12 @@
+function stringReverse(){
+let str ="testleaf";
+let rev="";
+
+for(let i=str.length;i>=0;i--){
+    rev =rev+str.charAt(i);
+}
+console.log(rev);
+}
+stringReverse()
+
+
--- VarLetConst.js
+++ VarLetConst.js
@@ -1,13 +1,27 @@
-// Global constant
+// Global constant with let
 const browserName = "Chrome";
 
-function getBrowserName() {
+function getBrowserName(browserName) {
     if (browserName === "Chrome") {
         let browserName = "Firefox"; // shadows the global variable
         console.log("Inside if block:", browserName);
     }
     // var is function-scoped
-    console.log("Outside block but inside function:", browserName);
+    console.log("1. Outside block but inside function:", browserName);
 }
 
-getBrowserName();
+getBrowserName(browserName);
+
+
+// Global constant with let
+const browName = "Chrome";
+
+function getBrowsersName(browName) {
+    if (browserName === "Edge") {
+        var browserName = "Opera"; // shadows the global variable
+    }
+    // var is function-scoped
+    
+}
+console.log("Outside block but inside function:", browserName);
+getBrowsersName(browName);
